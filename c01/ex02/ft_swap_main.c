42dd3709bc598531aa3b8a97cf877f95071bc42b
alexPic <alex.pic.42bcn@gmail.com>
2022-08-21 00:38:11 +0200

    [ADD] c01/ex02

---
+++ c01/ex02/ft_swap_main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	
+	*a = *b;
+	*b = temp;
+}
+
+
+int main()
+{
+	int	a;
+	int	b;
+	int	*ptra;
+	int	*ptrb;
+	a = 5;
+	b = 7;
+	ptra = &a;
+	ptrb = &b;
+	
+	printf("a = %d y b = %d\n", a, b);
+	ft_swap(ptra,ptrb);
+	printf("a = %d y b = %d\n", a, b);
+	return (0);
+}
