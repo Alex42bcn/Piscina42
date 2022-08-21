7672c2f458dc5b6a808d1eed9e500ac71b5286f3
alexPic <alex.pic.42bcn@gmail.com>
2022-08-21 22:31:44 +0200

    [FIX] ex07

--- c01/ex07/ft_rev_int_tab_main.c
+++ c01/ex07/ft_rev_int_tab_main.c
@@ -4,29 +4,23 @@ void	ft_rev_int_tab(int *tab, int size)
 {
 	int temp;
 	int i;
-	int mid;
 
 	i = 0;
-	mid = size / 2;
-	if (size % 2 == 0)
-		mid --;
-
-	while (i < mid)
+	while (i < (size / 2))
 	{
 		temp = tab[i];
 		tab[i] = tab[size - 1 - i];
 		tab[size - 1 - i] = temp;
 		i++;
 	}
-
 }
 
 int	main()
 {
-	int tab [] = {1,2,3,4,5,6};
-
-	ft_rev_int_tab(tab, 5);
-	printf("%d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]);
+	int	tab [] = {1,2,3,4,5};
+	int	size = 5;
+	ft_rev_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4]);
 	
 	return (0);
 }
