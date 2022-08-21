61a4975da80f906d977d45ed54daadcce0056148
alexPic <alex.pic.42bcn@gmail.com>
2022-08-22 00:33:46 +0200

    [FIX] Refactor ex08

--- c01/ex08/ft_sort_int_tab_main.c
+++ c01/ex08/ft_sort_int_tab_main.c
@@ -26,10 +26,14 @@ int main()
 {
 	int     tab [] = {3,4,2,5,1};
 	int     size = 5;
-
+	int     tab6 [] = {3,4,2,5,1,6};
+	int     size6 = 6;
 	ft_sort_int_tab(tab, size);
+	ft_sort_int_tab(tab6, size6);
 	
 	printf("%d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4]);
+
+printf("%d, %d, %d, %d, %d, %d", tab6[0], tab6[1], tab6[2], tab6[3], tab6[4], tab6[5]);
 
 	return (0);
 }
