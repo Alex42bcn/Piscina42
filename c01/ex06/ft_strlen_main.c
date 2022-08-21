b1f6682fcc892b8f0f13eb0c4cb8292bc9607cd2
alexPic <alex.pic.42bcn@gmail.com>
2022-08-21 22:15:27 +0200

    [ADD] refactor ex06 add ex07

---
+++ c01/ex06/ft_strlen_main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i_count;
+
+	i_count = 0;
+	while (str[i_count] != '\0')
+	{
+		i_count ++;
+	}
+	return (i_count);
+}
+
+int	main()
+{
+
+	char *str;
+
+	str = "hello es una prueba";
+	//ft_strlen(str);
+	printf("%d\n", ft_strlen(str));
+
+	return (0);
+}
