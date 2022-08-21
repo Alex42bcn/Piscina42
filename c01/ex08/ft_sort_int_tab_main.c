3146d52b1cb8926076d9f71f62a6cc36add39830
alexPic <alex.pic.42bcn@gmail.com>
2022-08-22 00:28:07 +0200

    [FIX] ex08 i add C02

--- c01/ex08/ft_sort_int_tab_main.c
+++ c01/ex08/ft_sort_int_tab_main.c
@@ -3,14 +3,12 @@
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int	j;
 	int	temp;
 
-	i = 0;
-	while (i < size)
+	while (size >= 0)
 	{
-		j = 0;
-		while (j < (size -1))
+		i = 0;
+		while (i < (size -1))
 		{
 			if (tab[i] > tab[i + 1])
 			{
@@ -18,10 +16,10 @@ void	ft_sort_int_tab(int *tab, int size)
 				tab[i] = tab [i + 1];
 				tab [i + 1] = temp;
 			}
+			i++;
 		}
-		j++;
+		size--;
 	}
-	i++;
 }
 
 int main()
