6ac51a440b1e665026691fd03a96e7cce2c2acd6
alexPic <alex.pic.42bcn@gmail.com>
2022-08-23 01:07:08 +0200

    [FIX] c02/ex06

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -8,7 +8,7 @@ int	ft_str_is_printable(char *str)
 	if (str[i] == '\0')
 		return (1);
 	while (str[i] != '\0')
-		if (str[i] >= 33 && str[i] <= 126)
+		if (str[i] >= 32 && str[i] <= 126)
 		i++;
 	else
 		return (0);
