1a7739bfb101a34b52132d7f42fa105c44aedf2a
alexPic <alex.pic.42bcn@gmail.com>
2022-08-22 13:10:06 +0200

    [FIX] Refactoring ex00

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -2,15 +2,25 @@
 
 char	*ft_strcpy(char *dest, char *src)
 {
-
-
+	int	i;
+	
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i ++;
+	}
+	return (dest);
 
 }
 
 int	main()
 {
+	char	src[] = "hello_test";
+	char	dest[] = "";
+	printf(":%s:\n", dest);
+	ft_strcpy(dest,src);
+	printf(":%s:\n", dest);
 
-
-
-
+	return (0);
 }
