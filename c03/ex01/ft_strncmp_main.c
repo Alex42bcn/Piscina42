df7c9c24ae6efce0abf2dd572af262e5aa1a39b1
alexPic <alex.pic.42bcn@gmail.com>
2022-08-24 01:12:57 +0200

    [ADD] c03/ex01

---
+++ c03/ex01/ft_strncmp_main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while((s1[i] != '\0' || s2[i] != 0) && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+			break;
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+int	main()
+{
+	char	s1[] = "gtg9sde";
+	char	s2[] = "gtg2sed";
+	printf("El valor de la comparacion: %d\n", ft_strncmp(s1, s2, 4));
+
+	char	s3[] = "gfzsde";
+	char	s4[] = "gfgsde";
+	printf("El valor de la comparacion: %d\n", ft_strncmp(s3, s4, 4));
+
+	char	s5[] = "bfbsed";
+	char	s6[] = "gfgsed";
+	printf("El valor de la comparacion: %d\n", ft_strncmp(s5, s6, 4));
+
+	return (0);
+}
