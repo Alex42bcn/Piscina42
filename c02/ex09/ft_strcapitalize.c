c1e948a4423f198a5d6b52af17cee232f2d09551
alexPic <alex.pic.42bcn@gmail.com>
2022-08-23 13:08:02 +0200

    [ADD] c02/ex09

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	check;
+
+	i = 0;
+	check = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (check == 1)
+				str[i] = str[i] - 32;
+				check = 0;
+		}
+		else if (str[i] >= 48 && str[i] <= 57)
+			check = 0;
+		else
+			check = 1;
+		i++;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char	str[100] = "salut, commi9tment :to %van? 42minut alex";
+	printf(":%s:\n", str);
+	printf(":%s:\n", ft_strcapitalize(str));
+
+	return (0);
+}
