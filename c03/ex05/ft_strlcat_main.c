6e20c09e662247a83f8437d2e1630143de432c46
alexPic <alex.pic.42bcn@gmail.com>
2022-08-25 15:18:36 +0200

    [FIX] C03/ex05

--- c03/ex05/ft_strlcat_main.c
+++ c03/ex05/ft_strlcat_main.c
@@ -16,7 +16,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	j;
 	unsigned int	dstsize;
 	unsigned int	dest_len;
-	int	src_len;
+	unsigned int	src_len;
 
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
@@ -25,32 +25,36 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	j = 0;
 	if (size == 0 || size <= dest_len)
 		return (dstsize);
-
-	while(i < (size - dest_len -1) && src[j] != '\0')  
+	while (j < (size - dest_len - 1) && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
-
 	return (dstsize);
 }
 
 int	main()
 {
-	char	first[50] = "This is ";
+	/*char	first[] = "This is ";
 	char	last[] = "a potentially long string";
 	unsigned int	r;
 	unsigned int	size;
 	
-	size = 65;
-	r = ft_strlcat(first,last,size);
+	size = 15;
+	//r = ft_strlcat(first,last,size);
 	printf("Lenght first str = %d\n", ft_strlen(first));
 	printf("Lenght last str = %d\n", ft_strlen(last));
+	r = ft_strlcat(first,last,size);
+
 	printf("Value returned: %d\n",r);
 
-	printf("%s\n", first);
+	printf("%s\n", first);*/
 
+	char src[] = "a potencially long string";
+	char dest [] = "This is ";
+	printf("%i \n", ft_strlcat(dest, src, 35));
+	printf("%s \n", dest);
 	return (0);
-			}
+}
---
+++ c03/ex05/test1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dlen;
+	unsigned int	slen;
+
+	i = 0;
+	dlen = ft_strlen(dest);
+	slen = ft_strlen(src);
+	j = dlen;
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
+}
+
+int main (void)
+{
+	char src[] = "a potencially long string";
+    	char dest [] = "This is ";
+	printf("%i \n", ft_strlcat(dest, src, 35));
+	printf("%s \n", dest);
+}
