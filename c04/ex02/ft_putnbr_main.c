72256cd22b9a3aa87930414ec9180608518dd79d
alexPic <alex.pic.42bcn@gmail.com>
2022-08-26 18:44:08 +0200

    [ADD] C04/ex02

---
+++ c04/ex02/ft_putnbr_main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		nb = nb * -1;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int main()
+{
+	ft_putnbr(-2147483648);
+	printf("\n");
+	ft_putnbr(3);
+	printf("\n");
+	ft_putnbr(2141);
+	printf("\n");
+	ft_putnbr(-2141);
+	printf("\n");
+	ft_putnbr(-2147483647);
+	printf("\n");
+	ft_putnbr(2147483647);
+
+	return (0);
+}
