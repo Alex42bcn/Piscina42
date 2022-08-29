7304f4faf44a82a10e316a7f627b63201b5c0917
Alex Picanyol <apicanyo@student.42barcelona.com>
2022-08-29 13:39:03 +0200

    [ADD] Refactor c05/ex06

---
+++ c05/ex06/ft_is_prime_main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime_main.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apicanyo <apicanyo@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 13:37:07 by apicanyo          #+#    #+#             */
+/*   Updated: 2022/08/29 13:37:32 by apicanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= (nb / 2))
+	{
+		if (!(nb % i))
+			return (0);
+		else
+			i += 1;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	printf("%d -> %d\n", -3965, ft_is_prime(-3965));
+	printf("%d -> %d\n", -12, ft_is_prime(-12));
+	printf("%d -> %d\n", 0, ft_is_prime(0));
+	printf("%d -> %d\n", 1, ft_is_prime(1));
+	printf("%d -> %d\n", 2, ft_is_prime(2));
+	printf("%d -> %d\n", 4219, ft_is_prime(4219));
+	return (0);
+}	
